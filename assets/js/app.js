4e4085f71d1f48de3865c85d5d664a62cc7371bf
dunyung1 <dshege4@gmail.com>
2019-08-25 09:11:14 +0300

    add ui-router extension and finish routings

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,23 @@
-var myApp = angular.module('myApp',['ngRoute']);
+var myApp = angular.module('myApp', ['ui.router']);
 
-myApp.config(['$routeProvider', function($routeProvider){
+myApp.config(function ($stateProvider, $urlRouterProvider) {
 
-    $routeProvider
-    .when('/home', {
-        templateUrl: 'views/home.html'
-    })
-    .when('/milestones', {
-        templateUrl: 'views/milestones.html'
-    })
-    .when('/madebymuthoni',{
-        templateUrl: 'views/madebymuthoni.html'
-    })
-    .when('/muthonispeaks',{
-        templateUrl: 'views/muthonispeaks.html'
-    })
-    .otherwise({
-        redirectTo: '/home'
-    })
-}]);
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'views/home.html'
+        })
+        .state('milestones', {
+            url: '/milestones',
+            templateUrl: 'views/milestones.html'
+        })
+        .state('madebymuthoni', {
+            url: '/madebymuthoni',
+            templateUrl: 'views/madebymuthoni.html'
+        })
+        .state('muthonispeaks', {
+            url: '/muthonispeaks',
+            templateUrl: 'views/muthonispeaks.html'
+        })
+});
